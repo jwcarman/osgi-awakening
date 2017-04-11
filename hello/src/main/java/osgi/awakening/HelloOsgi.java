507775f883133a5e839db54492d31f709c638502
James Carman <jwcarman@gmail.com>
2017-04-11 01:19:22 -0400

    Adding minimal example.

--- hello/src/main/java/osgi/awakening/HelloOsgi.java
+++
@@ -1,7 +0,0 @@
-package osgi.awakening;
-
-/**
- * Created by jcarman on 4/11/17.
- */
-public class HelloOsgi {
-}
---
+++ osgi-awakening-hello/src/main/java/osgi/awakening/HelloOsgi.java
@@ -0,0 +1,28 @@
+package osgi.awakening;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloOsgi implements BundleActivator {
+//----------------------------------------------------------------------------------------------------------------------
+// Fields
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloOsgi.class);
+
+//----------------------------------------------------------------------------------------------------------------------
+// BundleActivator Implementation
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        LOGGER.info("Hello, OSGi!");
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        LOGGER.info("Goodbye, OSGi!");
+    }
+}
